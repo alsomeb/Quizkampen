3fb2e063b710e854bffee8eed386b4242342da88
alsomeb <alsomeb@gmail.com>
2022-11-18 13:26:00 +0100

    Collab session

---
+++ src/main/java/org/quizkampen/client/Client.java
@@ -0,0 +1,111 @@
+package org.quizkampen.client;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/*
+Ver 1:
+    Ett simpelt bräde i Swing GUI (Client)
+    - 4 knappar, 1 label
+    - Current Score
+ */
+public class Client extends JFrame implements ActionListener{
+
+    private final JPanel welcomePanel = new JPanel();
+    private final JPanel waitingRoomPanel = new JPanel();
+    private final JPanel gamePanel = new JPanel();
+    private final JPanel resultPanel = new JPanel();
+
+    private final JLabel waitingRoomMsg = new JLabel("Waiting for 1 more player to connect..");
+    private final JLabel welcomeMsg = new JLabel("Welcome to Quizkampen!");
+    private final JButton startGameBtn = new JButton("Find a game for me");
+
+    // Network
+    private final int port = 12345;
+    private Socket socket;
+    private final String serverAdress = "127.0.0.1";
+    private BufferedReader in;
+    private PrintWriter out;
+    private String userName;
+
+    public Client() {
+        // Welcome Panel
+        loadWelcomePanel();
+
+        // Frame
+        this.add(welcomePanel);
+        this.setSize(800, 800);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.setTitle("Quizkampen");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    private void loadWelcomePanel() {
+
+        welcomePanel.setLayout(new BorderLayout());
+        welcomePanel.add(welcomeMsg, BorderLayout.NORTH);
+
+        startGameBtn.setFocusable(false);
+        startGameBtn.setFont(new Font("Sans-serif", Font.BOLD, 22));
+        startGameBtn.setHorizontalAlignment(JButton.CENTER);
+        welcomePanel.add(startGameBtn, BorderLayout.SOUTH);
+        startGameBtn.addActionListener(this);
+
+        welcomeMsg.setFont(new Font("Sans-serif", Font.BOLD, 22));
+        welcomeMsg.setPreferredSize(new Dimension(1000, 150));
+        welcomeMsg.setHorizontalAlignment(JLabel.CENTER);
+        welcomeMsg.setVerticalAlignment(JLabel.CENTER);
+    }
+
+    private void setWaitingRoomPanel() throws IOException {
+        socket = new Socket(serverAdress, port);
+
+        waitingRoomPanel.setLayout(new BorderLayout());
+        waitingRoomPanel.add(waitingRoomMsg, BorderLayout.CENTER);
+        waitingRoomMsg.setFont(new Font("Sans-serif", Font.BOLD, 22));
+        waitingRoomMsg.setHorizontalAlignment(JLabel.CENTER);
+        waitingRoomMsg.setVerticalAlignment(JLabel.CENTER);
+    }
+
+    private String prompt(String messageInPrompt) {
+        while (true) {
+            String name = JOptionPane.showInputDialog(messageInPrompt);
+            if (name == null) {
+                JOptionPane.showMessageDialog(null, "Måste fylla i namn!");
+            } else if (name.isBlank()) {
+                JOptionPane.showMessageDialog(null, "Namn Får ej va tomt!");
+            } else {
+                return name;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+            new Client();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == startGameBtn) {
+            userName = prompt("Enter player name");
+
+            // Felhantering görs innan detta nedan
+            welcomePanel.setEnabled(false);
+            try {
+                setWaitingRoomPanel();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+}
