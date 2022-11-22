a5aba0c642914059579d5bed6b0098370dcbd379
alsomeb <alsomeb@gmail.com>
2022-11-22 15:48:35 +0100

    - Collab Session Full Group
    - Client Server can talk
    
    - Gui work
    
    - Database Work
    
    - Refactoring

---
+++ src/main/java/org/quizkampen/server/DbEnum.java
@@ -0,0 +1,21 @@
+package org.quizkampen.server;
+
+public enum DbEnum {
+
+    PROGRAMMERING("Programmering"),
+    LITTERATURKONST("Litteraturkonst"),
+    GEOGRAFI("Geografi"),
+    HISTORIA("Historia");
+
+    private final String category;
+
+    DbEnum(String typ) {
+        this.category = typ;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
+
+
