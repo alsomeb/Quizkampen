067fbb63782c9d152f0838e5f0d6a7b8fb8ec159
alsomeb <alsomeb@gmail.com>
2022-11-23 12:25:02 +0100

    - Collab Session Full Group
    - Quiz can play 1 round and switch player

--- src/main/java/org/quizkampen/server/GameService.java
+++ src/main/java/org/quizkampen/server/GameService.java
@@ -138,64 +138,6 @@ public class GameService extends Thread {
             System.out.println("spel slut");
 
         }
-            /*String msgFromClient = "";
-
-
-            while (true) {
-                if (state == 0) {
-                    System.out.println("Startar state 0");
-                    quizPlayer1.sendResponseToClient(new Initiator(true));
-                    quizPlayer2.sendResponseToClient(new Initiator(true));
-                    state++;
-                    System.out.println("testar state initial");
-                }
-
-                if (state == 1) {
-                    System.out.println("Startar state 1");
-                    quizPlayer1.sendResponseToClient(new Response(db.getCategories()));
-                    quizPlayer1.sendResponseToClient("Skickat kategorier");
-                    System.out.println("Skickat kategorier");
-                    state++;
-                }
-
-                if (state == 2) {
-                    System.out.println("Startar state 2");
-                    msgFromClient = quizPlayer1.receiveFromClient();
-                    System.out.println(msgFromClient);
-                    state++;
-                }
-
-                if (state == 3) {
-                    msgFromClient = msgFromClient.toLowerCase();
-                    System.out.println("Startar state 3, skickar Frågor till player 1");
-                    switch (msgFromClient) {
-                        case "litteraturkonst" ->
-                                quizPlayer1.sendResponseToClient(new Response(db.getQuestionsListByName(DbEnum.LITTERATURKONST)));
-                        case "programmering" ->
-                                quizPlayer1.sendResponseToClient(new Response(db.getQuestionsListByName(DbEnum.PROGRAMMERING)));
-                        case "geografi" ->
-                                quizPlayer1.sendResponseToClient(new Response(db.getQuestionsListByName(DbEnum.GEOGRAFI)));
-                        case "historia" ->
-                                quizPlayer1.sendResponseToClient(new Response(db.getQuestionsListByName(DbEnum.HISTORIA)));
-                    }
-                    state++;
-                }
-                if(state == 4) {
-                    msgFromClient = quizPlayer1.receiveFromClient();
-                    int quizPlayer1CurrentScore = quizPlayer1.getScore();
-
-                    if(msgFromClient.equalsIgnoreCase("correct")) {
-                        System.out.println(msgFromClient);
-                        quizPlayer1CurrentScore++;
-                        quizPlayer1.setScore(quizPlayer1CurrentScore);
-                        System.out.println(quizPlayer1.getScore());
-                    }
-                }*/
-        //}
-        // TODO HITTA PÅ ETT BRA SÄTT ATT RÄKNA IHOP FULLSTÄNDIG POÄNG FÖR PLAYER 1
-        // TODO STARTA PLAYER 2, SÄTT PLAYER 1 I WAITING PANEL
-        // TODO Sätta kanske hela protokollet i en metod med en Player som in parameter
-
     }
 }
 
